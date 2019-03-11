48062d0206d80bfa12a51e0b134e8970d77a468d
lanminik <milannik91@hotmail.com>
2019-03-11 17:15:44 +0100

    mongo models event, user

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const qraphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
+const express = require("express");
+const bodyParser = require("body-parser");
+const qraphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
 
-app.use('/graphql', qraphqlHttp({
-  schema: buildSchema(`
+app.use(
+  "/graphql",
+  qraphqlHttp({
+    schema: buildSchema(`
     type Event {
       _id: ID!
       title: String!
       description: String!
       price: Float!
       date: String!
+    }
+
+    type User {
+      _id: ID!
+      email: String!
+      password: String
     }
 
     input EventInput {
@@ -26,12 +37,18 @@ app.use('/graphql', qraphqlHttp({
       date: String!
     }
 
+    input UserInput {
+      email: String!
+      password: String!
+    }
+
     type RootQuery {
       events: [Event!]!
     }
     
     type RootMutation {
       createEvent(eventInput: EventInput): Event
+      createUser(userInput: UserInput): User
     }
 
     schema {
@@ -39,24 +56,88 @@ app.use('/graphql', qraphqlHttp({
       mutation: RootMutation
     }
   `),
-  rootValue: {
-    events: () => {
-      return events;
-    },
-    createEvent: (args) => {
-      const event = {
-        _id: Math.random().toString(),
-        title: args.eventInput.title,
-        description: args.eventInput.description,
-        price: +args.eventInput.price,
-        date: new Date().toISOString()
+    rootValue: {
+      events: () => {
+        return Event.find()
+          .then(events => {
+            return events;
+          })
+          .catch(err => {
+            throw err;
+          });
+      },
+      createEvent: args => {
+        const event = new Event({
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: new Date(args.eventInput.date),
+          creator: "5c86875d37c978495d1bdcba"
+        });
+
+        let createdEvent;
+
+        return event
+          .save()
+          .then(result => {
+            createdEvent = result;
+            return User.findById("5c86875d37c978495d1bdcba");
+          })
+          .then(user => {
+            if (!user) {
+              throw new Error("User not found.");
+            }
+
+            user.createdEvents.push(event);
+            return user.save();
+          })
+          .then(result => {
+            return createdEvent;
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
+      },
+      createUser: args => {
+        return User.findOne({ email: args.userInput.email })
+          .then(user => {
+            if (user) {
+              throw new Error("User exists already.");
+            }
+
+            return bcrypt.hash(args.userInput.password, 12);
+          })
+          .then(hashedPassword => {
+            const user = new User({
+              email: args.userInput.email,
+              password: hashedPassword
+            });
+
+            return user.save();
+          })
+          .then(result => {
+            return { ...result._doc, password: null };
+          })
+          .catch(err => {
+            throw err;
+          });
       }
+    },
+    graphiql: true
+  })
+);
 
-      events.push(event);
-      return event;
-    }
-  },
-  graphiql: true
-}));
-
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@project0-ld84z.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
