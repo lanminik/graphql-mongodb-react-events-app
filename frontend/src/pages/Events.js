cdf962840042b71bc733302da2eb9ae7e6b26329
lanminik <milannik91@hotmail.com>
2019-03-20 17:11:45 +0100

    Add events modal

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,96 @@
 import React, { Component } from 'react';
 
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
+import './Events.css';
+
 class EventsPage extends Component {
+  state = {
+    creating: false
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.titleElRef = React.createRef();
+    this.priceElRef = React.createRef();
+    this.dateElRef = React.createRef();
+    this.descriptionElRef = React.createRef();
+  }
+
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
+
+  onCancelModalHandler = () => {
+    this.setState({ creating: false });
+  };
+
+  onConfirmModalHandler = () => {
+    this.setState({ creating: false });
+    const title = this.titleElRef.current.value;
+    const price = this.priceElRef.current.value;
+    const date = this.dateElRef.current.value;
+    const description = this.descriptionElRef.current.value;
+
+    const event = { title, price, date, description };
+
+    if (
+      title.trim().length === 0 ||
+      price.trim().length === 0 ||
+      date.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      return;
+    }
+
+    console.log(event)
+  };
+
   render() {
-    return <h1>Events page</h1>
+    return (
+      <>
+        {this.state.creating && <Backdrop />}
+        {this.state.creating && (
+          <Modal
+            title="Add event"
+            canCancel
+            canConfirm
+            onCancel={this.onCancelModalHandler}
+            onConfirm={this.onConfirmModalHandler}
+          >
+            <form>
+              <div className="form-control">
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" ref={this.titleElRef} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="price">Price</label>
+                <input type="number" id="price" ref={this.priceElRef} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="date">Date</label>
+                <input type="datetime-local" id="date" ref={this.dateElRef} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  id="description"
+                  rows="4"
+                  ref={this.descriptionElRef}
+                />
+              </div>
+            </form>
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Share your own events!</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>
+            Create Event
+          </button>
+        </div>
+      </>
+    );
   }
 }
 
